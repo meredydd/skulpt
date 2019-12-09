32ed3b4872a21a56d7ac189b8383ed648795d93e
Meredydd Luff <meredydd@anvil.works>
2019-12-09 16:27:24 +0000

    Get a good subset of the CPython fstring tests running

--- src/ast.js
+++ src/ast.js
@@ -2610,7 +2610,6 @@ function fstring_parse(str, start, end, raw, recurse_lvl, c, n) {
             idx = endIdx;
         }
     }
-    Sk.asserts.assert(values.length != 0);
     return [new Sk.astnodes.JoinedStr(values, LINENO(n), n.col_offset), idx];
 }
 
